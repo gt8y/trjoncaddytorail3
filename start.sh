41baff58b0059edbece04e001e418149152e99db
gt8y <101302945+gt8y@users.noreply.github.com>
2022-10-15 11:44:49 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@
 
 # Run Trojan-go
 tor &
-/usr/local/bin/trojan-go -config /usr/local/etc/trojan-go/config.yaml &
+/trojan-go -config /usr/local/etc/trojan-go/config.yaml &
 caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
