73a8845a0070a6455d080ecfba396361ac7ddaa6
gt8y <101302945+gt8y@users.noreply.github.com>
2022-10-15 11:53:58 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,25 @@
 #!/bin/sh
 
+# Trojan-go new configuration
+install -d /usr/local/etc/trojan-go
+cat << EOF > /usr/local/etc/trojan-go/config.yaml
+run-type: server
+local-addr: 0.0.0.0
+local-port: $PORT
+remote-addr: 127.0.0.1
+remote-port: 80
+log-level: 5
+password:
+    - $PASSWORD
+websocket:
+  enabled: true
+  path: /
+transport-plugin:
+  enabled: true
+  type: plaintext
+EOF
 
 # Run Trojan-go
 tor &
-/trojan-go -config /usr/local/etc/trojan-go/config.yaml &
+/usr/local/etc/trojan-go -config /usr/local/etc/trojan-go/config.yaml &
 caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
